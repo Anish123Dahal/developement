0339a573c8b2c6da18c5d957591a1a7b746354ec
Anish123Dahal <dahalanish002@gmail.com>
2026-04-10 11:00:55 +0530

    Add greeting and addition functions to app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 
+# app.py
+
+def greet(name):
+    return f"Hello, {name}! Welcome to AWS CI/CD Pipeline 🚀"
+
+def add_numbers(a, b):
+    return a + b
+
+def main():
+    print(greet("Anish"))
+
+    result = add_numbers(5, 7)
+    print(f"Addition Result: {result}")
+
+if __name__ == "__main__":
+    main()
